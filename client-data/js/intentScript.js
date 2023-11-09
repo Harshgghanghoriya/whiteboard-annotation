e23c9c38222b4ef30bcb09b5ff3529499cf76195
Harshgghanghoriya <108796471+Harshgghanghoriya@users.noreply.github.com>
2023-11-09 10:57:39 +0530

    9 November Annotation

---
+++ client-data/js/intentScript.js
@@ -0,0 +1,9 @@
+function launchScannerApp() {
+  console.log("U3RhcnRSZWNvcmRpbmc=");
+}
+
+function launchScreenshotScript() {
+  var screenshotEvent = new Event("ScreenshotToolTriggered");
+  document.dispatchEvent(screenshotEvent);
+  console.log("ScreenshotToolTriggered");
+}
